3302842b6faf11a5cecf8ec0c528ac2b9187f24a
Рустам <142333496+RayanToy@users.noreply.github.com>
2023-10-04 20:19:44 +0300

    Update 3.2 Регулярные выражения в Python 12.py

--- 3.2 Регулярные выражения в Python 12.py
+++ 3.2 Регулярные выражения в Python 12.py
@@ -1,3 +1,17 @@
+'''
+Вам дана последовательность строк.
+
+В каждой строке замените все вхождения подстроки "human" на подстроку "computer"﻿ и выведите полученные строки.
+
+Sample Input:
+
+I need to understand the human mind
+humanity
+Sample Output:
+
+I need to understand the computer mind
+computerity
+'''
 import sys
 import re
 
