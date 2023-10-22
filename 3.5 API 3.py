a4b2330f37328e95ce3dc71459b4ad7af1afb224
Рустам <142333496+RayanToy@users.noreply.github.com>
2023-10-22 17:15:17 +0300

    Update 3.5 API 3.py

--- 3.5 API 3.py
+++ 3.5 API 3.py
@@ -1,3 +1,4 @@
+'''
 В этой задаче вам необходимо воспользоваться API сайта numbersapi.com.
 
 Вам дается набор чисел. Для каждого из чисел необходимо узнать, существует ли интересный математический факт об этом числе.
@@ -24,3 +25,16 @@ Interesting
 Boring
 Interesting
 Boring
+'''
+
+import requests
+with open('dataset_24476_3.txt') as file:
+    lines = file.readlines()
+lines = [line.replace('\n', '') for line in lines]
+for line in lines:
+    res = requests.get(f'http://numbersapi.com/{line}/math?json=true')
+    data = res.json()
+    if not data['found']:
+        print('Boring')
+    else:
+        print('Interesting')
