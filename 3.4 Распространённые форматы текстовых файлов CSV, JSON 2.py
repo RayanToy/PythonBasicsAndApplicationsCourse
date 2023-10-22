b765532ead00e0d7bf3856bff324d7613f6b5ee1
Рустам <142333496+RayanToy@users.noreply.github.com>
2023-10-22 17:12:55 +0300

    Update 3.4 Распространённые форматы текстовых файлов CSV, JSON 2.py

--- 3.4 Распространённые форматы текстовых файлов CSV, JSON 2.py
+++ 3.4 Распространённые форматы текстовых файлов CSV, JSON 2.py
@@ -7,3 +7,13 @@
 
 Файл с данными: Crimes.csv
 '''
+import csv
+import pandas as pd
+
+df = pd.read_csv('Crimes.csv')
+df['Date'] = pd.to_datetime(df['Date'])
+df = df[df['Date'].dt.year == 2015]
+columns_to_keep = ['Date', 'Primary Type']
+df = df[columns_to_keep]
+unique_values = df['Primary Type'].value_counts()
+print(unique_values)
